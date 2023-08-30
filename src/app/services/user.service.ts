a8e959b8fdee289d2c1e794610fd3104c06f8b12
Marcel Ovelar <fovelar@pol.una.py>
2023-08-30 13:08:48 -0400

    full fronted

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import baserUrl from './helper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private httpClient: HttpClient) { }
+  
+  public anadirUsuario(user:any){
+    return this.httpClient.post(`${baserUrl}/usuarios`, user);    
+  }
+
+}
